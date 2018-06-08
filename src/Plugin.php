402aedac98503b7928f0e21ab347c6313ffb52af
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2018-06-08 16:52:53 -0300

    Update plugin

---
+++ src/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jefferson Simão Gonçalves
+ * Email: gerson.simao.92@gmail.com
+ * Date: 08/06/2018
+ * Time: 16:47
+ */
+
+namespace DataTables;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Class Plugin
+ *
+ * @author Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
+ *
+ * @package DataTables
+ */
+class Plugin extends BasePlugin
+{
+
+}
