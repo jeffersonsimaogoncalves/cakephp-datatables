000a662d5e415c2fd0752f5ba8480515d3e1ec84
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2018-07-12 22:56:50 -0300

    Bug fix

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@ use Cake\Routing\Router;
 
 Router::scope('/data-tables', function (RouteBuilder $routes) {
     if (!$routes->nameExists('data-tables:index:index')) {
-        $routes->connect('/length', 'DataTables.Index::length', ['_name' => 'data-tables:index:index']);
-        $routes->connect('/start', 'DataTables.Index::start', ['_name' => 'data-tables:index:index']);
+        $routes->connect('/length', 'DataTables.Index::length');
+        $routes->connect('/start', 'DataTables.Index::start');
     }
 });
